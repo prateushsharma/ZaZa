07694b4925f168ba46e7ebdfb907e88c319ba5e5
PRATEUSH SHARMA <115470025+prateushsharma@users.noreply.github.com>
2025-05-03 03:19:07 +0530

    Basic no code started

---
+++ nocode/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import FlowCanvas from './FlowCanvas';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>No-Code Flow Editor</h1>
+      <FlowCanvas />
+    </div>
+  );
+}
+
+export default App;
