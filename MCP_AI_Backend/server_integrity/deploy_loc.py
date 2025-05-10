fe65f3a15dc6b1f27fc84cdfbadb8f1761fc2314
RudrakshSJoshi <rudrakshjoshi2004@gmail.com>
2025-05-10 16:22:50 +0530

    UPDATED trading logic and INCLUDED 5 retries attempt on failed deployments

--- MCP_AI_Backend/server_integrity/deploy_loc.py
+++ MCP_AI_Backend/server_integrity/deploy_loc.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
                 continue
             
             decision = agent_code(data)
-            print(f"Buy: {decision}")
+            print(f"Decision: {decision}")
             # latest = data['candlesticks'][-1]
             # print(f"🕯️ Latest Candle - Close: {latest['close']}, Volume: {latest['volume']}")
             # print("---")
@@ -69,55 +69,58 @@ if __name__ == "__main__":
 """
 
 async def graph_to_code(uid, password=None):
-    user_folder = f"./user_assets/{uid}"
-    code_sync_path = os.path.join(user_folder, "code_sync.json")
+    try:
+        user_folder = f"./user_assets/{uid}"
+        code_sync_path = os.path.join(user_folder, "code_sync.json")
 
-    # Check if the user folder exists
-    if not os.path.exists(user_folder):
-        return {"status": "error", "message": "User folder does not exist", "code": 404}
-    
-    # Check if the code_sync.json file exists in the folder
-    if not os.path.isfile(code_sync_path):
-        return {"status": "error", "message": "code_sync.json file not found", "code": 404}
-    
-    # Read the code_sync.json file
-    with open(code_sync_path, 'r') as file:
-        code_sync_data = json.load(file)
+        # Check if the user folder exists
+        if not os.path.exists(user_folder):
+            return {"status": "error", "message": "User folder does not exist", "code": 404}
+        
+        # Check if the code_sync.json file exists in the folder
+        if not os.path.isfile(code_sync_path):
+            return {"status": "error", "message": "code_sync.json file not found", "code": 404}
+        
+        # Read the code_sync.json file
+        with open(code_sync_path, 'r') as file:
+            code_sync_data = json.load(file)
 
-    # Check if the "deploy_status" field exists in the json and its value
-    if code_sync_data.get("deploy_status", False):
-        return {"status": "error", "message": "The no code graph is already deployed and no changes are noticed", "code": 400}
+        # Check if the "deploy_status" field exists in the json and its value
+        if code_sync_data.get("deploy_status", False):
+            return {"status": "error", "message": "The no code graph is already deployed and no changes are noticed", "code": 400}
 
-    # Copy the content of data_config.json and preprocess using map_json
-    with open(user_folder + "/data_config.json", 'r') as file:
-        data_config = json.load(file)
+        # Copy the content of data_config.json and preprocess using map_json
+        with open(user_folder + "/data_config.json", 'r') as file:
+            data_config = json.load(file)
 
-    mapped_json = map_json(data_config)
+        mapped_json = map_json(data_config)
 
-    ### Here we will convert json to code, and then try to deploy it ###
-    req, imp, code = gen_code(mapped_json)
-    imp2 = imp
-    imp.append("\n\nprint(\"Hello World\")")
+        ### Here we will convert json to code, and then try to deploy it ###
+        req, imp, code = gen_code(mapped_json)
+        imp2 = imp
+        imp.append("\n\nprint(\"Hello World\")")
 
-    print(code)
+        print(code)
 
-    req_path = os.path.join(user_folder, "requirements.txt")
-    imp_path = os.path.join(user_folder, "initialise.py")
-    code_path = os.path.join(user_folder, "trading_code.py")
+        req_path = os.path.join(user_folder, "requirements.txt")
+        imp_path = os.path.join(user_folder, "initialise.py")
+        code_path = os.path.join(user_folder, "trading_code.py")
 
-    with open(req_path, 'w') as file:
-        file.write("\n".join(req))
+        with open(req_path, 'w') as file:
+            file.write("\n".join(req))
 
-    # Write each import on a new line
-    with open(imp_path, 'w') as file:
-        file.write("\n".join(imp))
+        # Write each import on a new line
+        with open(imp_path, 'w') as file:
+            file.write("\n".join(imp))
 
-    indented_code = "\n".join("    " + line for line in code.splitlines())
+        indented_code = "\n".join("    " + line for line in code.splitlines())
 
-    # Replace placeholder and combine
-    final_code = "\n".join(imp2) + "\n\n" + placeholder_code.replace("<<>>", indented_code)
+        # Replace placeholder and combine
+        final_code = "\n".join(imp2) + "\n\n" + placeholder_code.replace("<<>>", indented_code)
 
-    with open(code_path, 'w', encoding='utf-8') as file:
-        file.write(final_code)
+        with open(code_path, 'w', encoding='utf-8') as file:
+            file.write(final_code)
 
-    return {"status": "success", "message": "Deployed successfully", "code": 200}
+        return {"status": "success", "message": "Deployed successfully", "code": 200}
+    except Exception as e:
+        return {"status": "error", "message": str(e), "code": 500}
